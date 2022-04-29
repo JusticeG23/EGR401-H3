a064198b4c120e7a6659e4444edc904817d1c49d
Derek Bowman <derek.bowman@calbaptist.edu>
2022-04-29 14:33:44 -0700

    Committing video game project

--- Menu.js
+++ Menu.js
@@ -1,32 +1,42 @@
 function newgameoption() {
-    document.querySelector("h3").style.display = "block";
-    document.getElementById("newgameoption").style.display = "block";
-    document.getElementById("continueoption").style.display = "block";
-    document.getElementById("menu_list").setAttribute('class', 'disable');
-    document.getElementById("return").style.display = "block";
+    const newgameselection = document.querySelectorAll("h3, .newgameoption, .continueoption, .returnmenu");
+    for(let i = 0; i < newgameselection.length; i++) {
+       newgameselection[i].style.display = "block";
+    }
+
+    let menulist = document.getElementById("menu_list");
+    menulist.setAttribute('class', 'disable');
 }
 
 function newgameoption2() {
-    document.querySelector("h3").style.display = "block";
-    document.getElementById("newgameoption2").style.display = "block";
-    document.getElementById("continueoption2").style.display = "block";
-    document.getElementById("menu_list").setAttribute('class', 'disable');
-    document.getElementById("return").style.display = "block";
+
+    const newgameselection2 = document.querySelectorAll("h3, .newgameoption2, .continueoption2, .returnmenu");
+    for(let i = 0; i < newgameselection2.length; i++) {
+       newgameselection2[i].style.display = "block";
+    }
+
+    let menulist = document.getElementById("menu_list");
+    menulist.setAttribute('class', 'disable');
 }
 
 function aboutlevels() {
-    document.querySelector("h4").style.display ="block";
-    document.getElementById("about").style.display = "block";
-    document.getElementById("menu_list").setAttribute('class', 'disable');
-    document.getElementById("return").style.display = "block";
+
+    const aboutlevels = document.querySelectorAll("h4, .aboutlevels, .returnmenu");
+    for(let i = 0; i < aboutlevels.length; i++) {
+       aboutlevels[i].style.display = "block";
+    }
+
+    let menulist = document.getElementById("menu_list");
+    menulist.setAttribute('class', 'disable');
 }
 
 function options() {
-    document.querySelector("h5").style.display ="block";
-    document.getElementById("screenbrightness").style.display = "block";
-    document.getElementById("musiconoff").style.display = "block";
+    const elements = document.querySelectorAll("h5, .screenbrightness, .musiconoff, .returnmenu");
+    for(let i = 0; i < elements.length; i++) {
+        elements[i].style.display = "block";
+    }
+
     document.getElementById("menu_list").setAttribute('class', 'disable');
-    document.getElementById("return").style.display = "block";
     document.getElementById("musiconoption").style.display = "block";
     document.getElementById("musicoffoption").style.display = "block";
     document.getElementById("screenslider").style.display = "block";
@@ -35,27 +45,30 @@ function options() {
 }
 
 function returnmenu() {
-    document.querySelector("h3").style.display = "none";
-    document.querySelector("h4").style.display ="none";
-    document.querySelector("h5").style.display = "none";
-    document.getElementById("newgameoption").style.display = "none";
-    document.getElementById("continueoption").style.display = "none";
-    document.getElementById("newgameoption2").style.display = "none";
-    document.getElementById("continueoption2").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("screenbrightness").style.display = "none";
-    document.getElementById("musiconoff").style.display = "none";
+    const htags = document.querySelectorAll("h3, h4, h5");
+    for(let i = 0; i < htags.length; i++) {
+        htags[i].style.display = "none";
+    }
+
+    const newgame = document.querySelectorAll(".newgameoption, .continueoption, .newgameoption2, .continueoption2");
+    for(let i = 0; i < newgame.length; i++) {
+       newgame[i].style.display = "none";
+    }
+
+    const submenu = document.querySelectorAll(".aboutlevels, .screenbrightness, .musiconoff, .returnmenu");
+    for(let i = 0; i < submenu.length; i++) {
+        submenu[i].style.display = "none";
+    }
+
     document.getElementById("musiconoption").style.display = "none";
     document.getElementById("musicoffoption").style.display = "none";
     document.getElementById("screenslider").style.display = "none";
-    document.getElementById("return").style.display = "none";
     document.getElementById("menu_list").setAttribute('class', 'enable');
-    document.getElementById("menu").setAttribute('class', null);
-    document.getElementById("menu").setAttribute('class', 'noblur');
     document.querySelector('label[for = "musiconoption"]').style.display = "none";
     document.querySelector('label[for = "musicoffoption"]').style.display = "none";
 }
 
+/* Function to turn music on */
 const on = document.getElementById("musiconoption");
 const audioon = document.getElementById("audio");
 function musicon() {
@@ -63,6 +76,7 @@ function musicon() {
 }
 on.addEventListener('click', musicon);
 
+/* Function to pause music */
 const off = document.getElementById("musicoffoption");
 const audiooff = document.getElementById("audio");
 function musicoff() {
@@ -70,6 +84,7 @@ function musicoff() {
 }
 off.addEventListener('click', musicoff);
 
+/* Function to adjust the screen slider */
 const background = document.querySelector(".background2");
 const rangeslide = document.getElementById("screenslider");
 function brightnessLevel() {
@@ -77,6 +92,7 @@ function brightnessLevel() {
 }
 rangeslide.addEventListener('click', brightnessLevel);
 
+/* Function to blur sub menu to 15px */
 const menu = document.querySelector(".background");
 const submenu = document.getElementById("menu_list");
 function blursubmenu() {
@@ -86,8 +102,9 @@ function blursubmenu() {
 }
 submenu.addEventListener('click', blursubmenu);
 
+/* Function to blur back to 0px */
 const backgnd = document.querySelector(".background");
-const retn = document.getElementById("return");
+const retn = document.querySelector(".returnmenu");
 function blurmenu() {
     backgnd.classList.remove("returnblur");
     backgnd.offsetWidth;
